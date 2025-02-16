6987daa56f4b7aee3c0c9023caa25efa4f295ead
iBarzha <grytcaie@gmail.com>
2025-02-16 16:03:36 +0200

    add modal windows

--- database/urls.py
+++ database/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import execute_sql, list_tables, home
+from .views import execute_sql, list_tables, home, create_table, delete_table
 
 urlpatterns = [
     path('tables/', list_tables, name='list_tables'),
     path('execute_sql/', execute_sql, name='execute_sql'),
     path('', home, name='home'),
+    path('create_table/', create_table, name='create_table'),
+    path('delete_table/', delete_table, name='delete_table'),
 ]
--- database/views.py
+++ database/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.db import connection
-from .models import DatabaseTable
 
 def execute_sql(request):
     if request.method == "POST":
@@ -13,9 +12,43 @@ def execute_sql(request):
         except Exception as e:
             return JsonResponse({'status': 'error', 'message': str(e)})
 
+
 def list_tables(request):
-    tables = DatabaseTable.objects.all().values("name", "columns_count", "created_at")
-    return JsonResponse(list(tables), safe=False)
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = cursor.fetchall()
+
+    table_list = []
+    for table in tables:
+        table_name = table[0]
+        cursor.execute(f"PRAGMA table_info({table_name})")
+        columns_count = len(cursor.fetchall())
+        table_list.append({
+            'name': table_name,
+            'columns_count': columns_count,
+        })
+
+    return JsonResponse(table_list, safe=False)
 
 def home(request):
     return render(request, 'index.html')
+
+def create_table(request):
+    if request.method == "POST":
+        table_name = request.POST.get('table_name', '')
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute(f"CREATE TABLE {table_name} (id INTEGER PRIMARY KEY AUTOINCREMENT)")
+            return JsonResponse({'status': 'success', 'message': f"Таблиця '{table_name}' створена!"})
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': str(e)})
+
+def delete_table(request):
+    if request.method == "POST":
+        table_name = request.POST.get('table_name', '')
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute(f"DROP TABLE {table_name}")
+            return JsonResponse({'status': 'success', 'message': f"Таблиця '{table_name}' видалена!"})
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': str(e)})
